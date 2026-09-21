8d24c49ee2ff45aa58a9a107f579fa3e4a304b4f
Luizy <110498947+luizypruss@users.noreply.github.com>
2026-09-21 19:38:49 -0300

    Add C++ program to filter stopwords from text file
    Implement a C++ program to read a text file, filter out stopwords, and save the result to a new file.

---
+++ Aula 21.09/Desafio em arquivo IA generativa.cpp
@@ -0,0 +1,16 @@
+Desafio em arquivo txt  (LER ARQUIVOS E ESCREVER)
+Fazer um programa em C ou C++ que:
+i) Abra e leia um arquivo txt contendo um texto qualquer (CARREGAR)
+ii) Analise palavra por palavra do arquivo (LER)
+iii) compare a palavra lida do arquivo, com lista de stopwords = [A,O,AS,OS]
+iv) salve toda e qualquer palavras em um arquivo de saída, chamdado arquivoSemStopWords.txt
+
+  A ideia geral e principal é: 
+i) carrega os stopWords do arquivo stopWords.txt em uma lista de stopwords. (blacklist: arquivo texto onde colocamos as palavras bloquadas)
+ii) ler aquivo  e suas palavras
+iii) comparar a palavra lida com as palavras na lista de stopWords
+iv) descarregar em arquivo de saída as palavras do texto que não foram stopWods (deixar só essência da coisa)
+
+(Serão 3 arquivos stopwords .txt e  dele criar uma lista na memória, um [vetor], criar 
+
+cultura in [a,as,e,o,os,um,uns,uma,umas]
